6ec863c647fb14c69281ffcf420fb2e720f52e2d
Al Palanuwech <ap689@cornell.edu>
2023-06-30 14:25:21 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+/**
+ * Merges 2 sorted integer arrays into a single, sorted array
+ * @param collection_1 - A sorted integer array
+ * @param collection_2 - A sorted integer array
+
+ */
+export function merge (collection_1: number[] , collection_2:number[] ) : number []{
+
+    let ptr1:number=0;
+    let ptr2:number=0;
+
+    let mergedList:number[]=new Array(collection_1.length+collection_2.length)
+
+    while (ptr1<collection_1.length && ptr2<collection_2.length){
+        // add smallest element first
+        if (collection_1[ptr1] < collection_2[ptr2]){
+            mergedList[ptr1+ptr2]=collection_1[ptr1]
+            ptr1++
+        }
+        else{
+            mergedList[ptr1+ptr2]=collection_2[ptr2]
+            ptr2++
+        }
+    }
+    // add remaining elements
+    while (ptr1<collection_1.length){
+        mergedList[ptr1+ptr2]=collection_1[ptr1]
+        ptr1++
+    }
+
+    while (ptr2<collection_2.length){
+        mergedList[ptr1+ptr2]=collection_2[ptr2]
+        ptr2++
+    }
+    
+    return mergedList
+}
+
+
---
+++ test/index.test.ts
@@ -0,0 +1,20 @@
+import { merge } from '../src/index';
+
+describe('testing merge function', () => {
+    test('2 empty arrays', () => {
+      expect(merge([],[])).toEqual([]);
+    });
+    test('non overlapping array', () => {
+      expect(merge([1,2,3],[4,5,6])).toEqual([1,2,3,4,5,6]);
+    });
+    test('overlapping array', () => {
+      expect(merge([1,3,5,7],[2,4,5,6])).toEqual([1,2,3,4,5,5,6,7]);
+    });
+    test('array1 remaining elements', () => {
+      expect(merge([1,5,10,11,12],[2,3])).toEqual([1,2,3,5,10,11,12]);
+    });
+    test('array2 remaining elements', () => {
+      expect(merge([1,5],[2,3,6,7,8])).toEqual([1,2,3,5,6,7,8]);
+    });
+    
+  });
